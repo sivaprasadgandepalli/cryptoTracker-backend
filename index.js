275584859b872625704615ef1472358e6d3d8849
Siva Prasad Gandepalli <sivaprasadgandepalli@gmail.com>
2025-02-22 21:14:52 +0530

    cors orgin added

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const routes = require("./routes/cryptoRoutes");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors({
-    origin: "https://cryptotracker-backend-ayh3.onrender.com"
-}));
+const allowedOrigins = [
+    "http://localhost:3000", 
+    "https://your-frontend-domain.com", 
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }));
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
