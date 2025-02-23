c007b5aab7e49137de652eec5065d00ac893e533
Siva Prasad Gandepalli <sivaprasadgandepalli@gmail.com>
2025-02-23 15:23:30 +0530

    origins added for cors

--- index.js
+++ index.js
@@ -7,27 +7,29 @@ require("dotenv").config();
 
 app.use(express.json());
 const allowedOrigins = [
-    "http://localhost:3000", 
-    "https://crypto-tracker-mu-umber.vercel.app", 
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  }));
+  "http://localhost:3000",
+  "https://crypto-tracker-mu-umber.vercel.app",
+  "https://www.crypto-tracker-mu-umber.vercel.app", // Add subdomain if needed
+  "http://localhost:5000", // Add other origins if needed
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+}));
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-    return res.send("get request.")
+  return res.send("get request.")
 })
 
 const port = process.env.PORT || 8000;
 app.listen(port, (err) => {
-    if (err) console.log(err);
-    console.log(`server is running at port: ${port}`);
+  if (err) console.log(err);
+  console.log(`server is running at port: ${port}`);
 })
